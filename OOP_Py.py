e31da37c637958209b1f2a84671b8b411df8a0f4
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-04-20 10:07:39 +0400

    !

--- OOP_Py.py
+++ OOP_Py.py
@@ -5,6 +5,33 @@ class Person:
         self.name = name
         self.age = age
 
+    def print_info(self):
+        print(f"Person name: {self.name}\nPerson age: {self.age}")
+
+    def person_say(self, message):
+        print(f"{self.name} say: \"{message}\"")
+
+    def __del__(self):
+        print("Удалён человек с именем", self.name)
+
+
 tom = Person("Tom", 22)
+print(tom.name)
+print(tom.age)
+tom.age = 37
+print(tom.age)
+
+tom.print_info()
+tom.person_say("Hello, friends")
+
+def create_person():
+    bob = Person("Bobby", 35)
+
+create_person()
+print("Конец программы")
+
+
+
+
 
 
