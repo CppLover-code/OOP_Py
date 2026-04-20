27632f8a62ae8ac01405694f0624a2b84a67e1eb
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-04-20 09:55:44 +0400

    !

--- OOP_Py.py
+++ OOP_Py.py
@@ -1,7 +1,10 @@
 class Person:
     # конструктор
-    def __init__(self):
+    def __init__(self, name, age):
         print("Создание объекта Person")
+        self.name = name
+        self.age = age
 
-tom = Person()
+tom = Person("Tom", 22)
+
 
