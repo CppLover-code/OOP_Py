c150929ba89193fbb134c196b3f6f71abec25c3f
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-04-21 11:46:05 +0400

    !

--- Ranges, Dictionaries.py
+++ Ranges, Dictionaries.py
@@ -83,3 +83,116 @@ if key in users:
 else:
     print("Элемент не найден")
 
+# Также для получения элементов можно использовать метод get
+users = {
+    "+11111111": "Tom",
+    "+33333333": "Bob",
+    "+55555555": "Alice"
+}
+ 
+user1 = users.get("+55555555")
+print(user1)    # Alice
+user2 = users.get("+33333333", "Unknown user")
+print(user2)    # Bob
+user3 = users.get("+44444444", "Unknown user")
+print(user3)    # Unknown user
+
+# Удаление - оператор del
+
+users = {
+    "+11111111": "Tom",
+    "+33333333": "Bob",
+    "+55555555": "Alice"
+}
+ 
+del users["+55555555"]
+print(users)    # { "+11111111": "Tom", "+33333333": "Bob"}
+
+# Но стоит учитывать, что если подобного ключа не окажется в словаре
+# то будет выброшено исключение KeyError. Поэтому опять же перед
+# удалением желательно проверять наличие элемента с данным ключом
+
+users = {
+    "+11111111": "Tom",
+    "+33333333": "Bob",
+    "+55555555": "Alice"
+}
+ 
+key = "+55555555"
+if key in users:
+    del users[key]
+    print(f"Элемент с ключом {key} удален")
+else:
+    print("Элемент не найден")
+
+
+# Удаление - метод pop()
+users = {
+    "+11111111": "Tom",
+    "+33333333": "Bob",
+    "+55555555": "Alice"
+}
+key = "+55555555"
+user = users.pop(key)
+print(user)     # Alice
+ 
+user = users.pop("+4444444", "Unknown user")
+print(user)     # Unknown user
+
+users.clear() # удаление всех элементов
+
+# Копирование и объединение словарей
+# Метод copy() копирует содержимое словаря, возвращая НОВЫЙ словарь
+
+users = {"+1111111": "Tom", "+3333333": "Bob", "+5555555": "Alice"}
+students = users.copy()
+print(students)     # {"+1111111": "Tom", "+3333333": "Bob", "+5555555": "Alice"}
+
+# Метод update() объединяет два словаря:
+users = {"+1111111": "Tom", "+3333333": "Bob"}
+ 
+users2 = {"+2222222": "Sam", "+6666666": "Kate"}
+users.update(users2)
+ 
+print(users)    # {"+1111111": "Tom", "+3333333": "Bob", "+2222222": "Sam", "+6666666": "Kate"}
+print(users2)   # {"+2222222": "Sam", "+6666666": "Kate"}
+
+# Перебор словаря
+users = {
+    "+11111111": "Tom",
+    "+33333333": "Bob",
+    "+55555555": "Alice"
+}
+for key in users:
+    print(f"Phone: {key}  User: {users[key]} ")
+
+for key, value in users.items():
+    print(f"Phone: {key}  User: {value} ")
+# Метод items() возвращает набор кортежей. Каждый кортеж 
+# содержит ключ и значение элемента, которые при переборе 
+# мы тут же можем получить в переменные key и value.
+
+# Перебор ключей
+for key in users.keys():
+    print(key)
+
+# КОМПЛЕКСНЫЕ СЛОВАРИ
+users = {
+    "Tom": {
+        "phone": "+971478745",
+        "email": "tom12@gmail.com"
+    },
+    "Bob": {
+        "phone": "+876390444",
+        "email": "bob@gmail.com",
+        "skype": "bob123"
+    }
+}
+# В данном случае значение каждого элемента словаря в свою очередь
+# представляет отдельный словарь.
+# Для обращения к элементам вложенного словаря соответственно
+# необходимо использовать два ключа:
+old_email = users["Tom"]["email"]
+users["Tom"]["email"] = "supertom@gmail.com"
+print(users["Tom"])     # { phone": "+971478745", "email": "supertom@gmail.com }
+
